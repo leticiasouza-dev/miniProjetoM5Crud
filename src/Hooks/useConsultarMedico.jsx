a0b92270a7f7ac72a6fd02aa0dd8d4fe1f8a4113
leticiaSouzaDev <lec667121@gmail.com>
2024-09-19 15:37:44 -0300

    feat: solving bug with login page

--- src/Hooks/useConsultarMedico.jsx
+++ src/Hooks/useConsultarMedico.jsx
@@ -1,27 +1,19 @@
 import axios from "axios";
 
-const API_URL = 'https://projetofinalm4-22ux.onrender.com/api/medico/all';
+const API_URL = 'https://projetofinalm4-22ux.onrender.com/api/medico/verificar';
 
 export const useConsultarMedico = () => {
     
     const consultaMedico = async (email, telefone) => {
         try {
-            //obtendo todos os médicos
-            const response = await axios.get(API_URL);
-            const medicos = response.data; 
+            const response = await axios.get(API_URL, {
+                params: { email, telefone }  // Envia os parâmetros corretamente
+            });
 
-            const medicoEncontrado = medicos.find(medico => 
-                medico.Email === email && medico.Telefone === telefone
-            );
-
-            if (medicoEncontrado) {
-                return medicoEncontrado;
-            } else {
-                return null; // Se não encontrou nenhum médico
-            }
+            return response.data;
         } catch (error) {
             console.error('Erro ao consultar médico:', error);
-            return null; // Em caso de erro, retorna null
+            return null;
         }
     };
 
